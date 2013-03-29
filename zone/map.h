301c26b9bbd42a2479463dc68909602cf9275894
Derision <DerisionEQ@gmail.com>
2013-03-29 20:53:33 +0000

    Made all Map methods which are currently not referenced outside Map, private, so I can see what I need to implement in an RCM based Map class.

--- zone/map.h
+++ zone/map.h
@@ -47,16 +47,6 @@ public:
 	Map();
 	~Map();
 	
-	bool loadMap(FILE *fp);
-	
-	//the result is always final, except special NODE_NONE
-	NodeRef SeekNode( NodeRef _node, float x, float y ) const;
-	
-	//these are untested since rewrite:
-	int *SeekFace( NodeRef _node, float x, float y );
-	float GetFaceHeight( int _idx, float x, float y ) const;
-	
-	bool LocWithinNode( NodeRef _node, float x, float y ) const;
 	
 	//nodes to these functions must be final
 	inline bool LineIntersectsNode( VERTEX start, VERTEX end, VERTEX *result, FACE **on = NULL) const
@@ -66,33 +56,35 @@ public:
 	bool LineIntersectsFace( PFACE cface, VERTEX start, VERTEX end, VERTEX *result) const;
 	inline float FindBestZ(VERTEX start, VERTEX *result, FACE **on = NULL) const { return FindBestZ(GetRoot(), start, result, on); }
 	bool LineIntersectsZone(VERTEX start, VERTEX end, float step, VERTEX *result, FACE **on = NULL) const;
-		
-//	inline unsigned int		GetVertexNumber( ) {return m_Vertex; }
-	inline uint32		GetFacesNumber( ) const { return m_Faces; }
-//	inline PVERTEX	GetVertex( int _idx ) {return mFinalVertex + _idx;	}
 	inline PFACE		GetFace( int _idx) {return mFinalFaces + _idx;		}
+	bool LineIntersectsZoneNoZLeaps(VERTEX start, VERTEX end, float step_mag, VERTEX *result, FACE **on);
+	bool CheckLosFN(VERTEX myloc, VERTEX oloc);
+
+private:
+	float FindBestZ( NodeRef _node, VERTEX start, VERTEX *result, FACE **on = NULL) const;
+	bool LineIntersectsNode( NodeRef _node, VERTEX start, VERTEX end, VERTEX *result, FACE **on = NULL) const;
+	//the result is always final, except special NODE_NONE
+	NodeRef SeekNode( NodeRef _node, float x, float y ) const;
+	//these are untested since rewrite:
+	int *SeekFace( NodeRef _node, float x, float y );
+	float GetFaceHeight( int _idx, float x, float y ) const;
+	bool LocWithinNode( NodeRef _node, float x, float y ) const;
+	inline uint32		GetFacesNumber( ) const { return m_Faces; }
 	inline PFACE		GetFaceFromlist( int _idx) {return &mFinalFaces[ mFaceLists[_idx] ];	}
 	inline NodeRef		GetRoot( ) const { return MAP_ROOT_NODE; }
 	inline PNODE		GetNode( NodeRef r ) { return( mNodes + r ); }
-	
 	inline float GetMinX() const { return(_minx); }
 	inline float GetMaxX() const { return(_maxx); }
 	inline float GetMinY() const { return(_miny); }
 	inline float GetMaxY() const { return(_maxy); }
 	inline float GetMinZ() const { return(_minz); }
 	inline float GetMaxZ() const { return(_maxz); }
-	bool LineIntersectsZoneNoZLeaps(VERTEX start, VERTEX end, float step_mag, VERTEX *result, FACE **on);
 	float FindClosestZ(VERTEX p ) const;
-	bool CheckLosFN(VERTEX myloc, VERTEX oloc);
+	bool loadMap(FILE *fp);
 
-private:
-	float FindBestZ( NodeRef _node, VERTEX start, VERTEX *result, FACE **on = NULL) const;
-	bool LineIntersectsNode( NodeRef _node, VERTEX start, VERTEX end, VERTEX *result, FACE **on = NULL) const;
-//	unsigned long m_Vertex;
 	uint32 m_Faces;
 	uint32 m_Nodes;
 	uint32 m_FaceLists;
-//	PVERTEX mFinalVertex;
 	PFACE mFinalFaces;
 	PNODE mNodes;
 	uint32 *mFaceLists;
@@ -106,8 +98,6 @@ private:
 	static void Normalize(VERTEX *p);
 	RaycastMesh *rm;
 
-//	void	RecLoadNode( PNODE	_node, FILE *l_f );
-//	void	RecFreeNode( PNODE	_node );
 };
 
 #endif
